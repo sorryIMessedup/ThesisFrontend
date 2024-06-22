c8d0ab21cb2c0b4d988488b524f32f2f33f42137
Cheng Tan <reg_eax@outlook.com>
2024-06-22 13:29:39 +0800

    完成审批开题报告界面

--- src/components/Home/ProposalManage2.vue
+++ src/components/Home/ProposalManage2.vue
@@ -1,13 +1,12 @@
 <template>
     <el-card>
       <div style="display: flex; align-items: center; justify-content: space-between;">
-        <h1>申请我的</h1>
+        <h1>我审批的开题报告</h1>
       </div>
-      <span>您应在<b>2023年11月17日</b>前确定学生的导师，审阅《本科生毕业论文指导老师意见表》。</span><br>
-      <span>逾期将无法操作，即无法通过或拒绝任何同学的申请。</span>
+      <span>您应在<b>2023年12月27日</b>前完成审阅《本科生毕业论文（设计）开题报告》。</span><br>
       <el-divider />
-      <el-table stripe :data="applicationData.slice((pageNum - 1) * pageSize, pageNum * pageSize)" style="" :border="false">
-        <el-table-column fixed prop="applicationId" label="申请ID" width="100" />
+      <el-table stripe :data="proposalData.slice((pageNum - 1) * pageSize, pageNum * pageSize)" style="" :border="false">
+        <el-table-column fixed prop="proposalId" label="开题报告ID" width="100" />
         <el-table-column prop="studentId" label="学生ID" width="150" />
         <el-table-column prop="teacherPass" label="状态" width="100" />
         <el-table-column prop="" label="操作" min-width="270">
@@ -20,7 +19,7 @@
       </el-table>
       <br>
       <el-pagination v-model:current-page="pageNum" v-model:page-size="pageSize" :page-sizes="[5, 10, 20, 50]"
-        layout="sizes, prev, pager, next" :total="applicationData.length" @size-change="handleSizeChange"
+        layout="sizes, prev, pager, next" :total="proposalData.length" @size-change="handleSizeChange"
         @current-change="handleCurrentChange" />
     </el-card>
   </template>
@@ -30,7 +29,7 @@
   import { onMounted, ref } from 'vue';
   import { ElMessage, ElMessageBox } from 'element-plus';
   
-  const applicationData = ref([])
+  const proposalData = ref([])
   const pageNum = ref(1)
   const pageSize = ref(5)
   const canApply = ref(false)
@@ -46,26 +45,26 @@
     let token = window.localStorage.getItem('token')
     axios({
       method: 'get',
-      url: '/api/application/get_by_teacher',
+      url: '/api/proposals/get_by_teacher',
       params: { teacher_id: window.localStorage.getItem('uid') },
       headers: { 'Authorization': `Bearer ${token}` }
     }).then(function (res) {
-      ElMessage.success('获取所有申请成功')
-      for (let item of res.data.application_list) {
+      ElMessage.success('获取所有开题报告成功')
+      for (let item of res.data) {
         if (item.teacherPass === 0) item.teacherPass = '未处理'
         if (item.teacherPass === 1) item.teacherPass = '已通过'
         if (item.teacherPass === 2) item.teacherPass = '已拒绝'
       }
-      applicationData.value = res.data.application_list
+      proposalData.value = res.data
       console.log(res)
     }, err => {
-      ElMessage.error('获取所有申请失败')
+        ElMessage.success('获取所有开题报告成功')
       console.log(err)
     })
     let currentDate = new Date()
-    let applicationEndDate = new Date('2023-11-17T00:00:01')
-    if (currentDate > applicationEndDate) {
-      ElMessageBox.alert('您已超过时限，无法成为更多学生的导师了！', '警告', {
+    let proposalEndDate = new Date('2023-11-17T00:00:01')
+    if (currentDate > proposalEndDate) {
+      ElMessageBox.alert('您已超过时限，无法审批开题报告了！', '警告', {
         confirmButtonText: 'OK',
       })
       canApply.value = true
@@ -77,8 +76,8 @@
     let token = window.localStorage.getItem('token')
     axios({
       method: 'post',
-      url: '/api/application/pass',
-      params: { application_id: row.applicationId },
+      url: '/api/proposal/pass',
+      params: { proposal_id: row.proposalId },
       headers: { 'Authorization': `Bearer ${token}` }
     }).then(function (res) {
       ElMessage.success('通过成功')
@@ -94,8 +93,8 @@
     let token = window.localStorage.getItem('token')
     axios({
       method: 'post',
-      url: '/api/application/refuse',
-      params: { application_id: row.applicationId },
+      url: '/api/proposal/refuse',
+      params: { proposal_id: row.proposalId },
       headers: { 'Authorization': `Bearer ${token}` }
     }).then(function (res) {
       ElMessage.success('拒绝成功')
@@ -111,15 +110,15 @@
     let token = window.localStorage.getItem('token')
     axios({
       method: 'get',
-      url: '/api/application/getPDF',
-      params: { application_id: row.applicationId },
+      url: '/api/proposal/getPDF',
+      params: { aproposal_id: row.proposalId },
       responseType: 'blob',
       headers: { 'Authorization': `Bearer ${token}`, }
     }).then(function (res) {
       const link = document.createElement('a');
       const blob = new Blob([res.data]);
       link.href = window.URL.createObjectURL(blob);
-      link.download = `第${row.applicationId}号申请.pdf`;
+      link.download = `第${row.proposalId}号申请.pdf`;
       link.style.display = 'none';
       document.body.appendChild(link);
       link.click();
